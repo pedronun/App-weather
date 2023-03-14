b52f5d3d6da94c02ab4f6a93c170cbafec66e26d
Pedro Lucas <pedronun@protonmail.com>
2023-03-14 18:42:32 -0300

    feat: finalizado app

--- src/screens/Map/Map.tsx
+++ src/screens/Map/Map.tsx
@@ -1,10 +1,21 @@
-import { SafeAreaView, Text } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView, Text } from "react-native";
 
 const Map = () => {
   return (
-    <SafeAreaView>
-      <Text>Map</Text>
-    </SafeAreaView>
+    <LinearGradient colors={["#2E335A", "#1C1B33"]} style={{ flex: 1 }}>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color: "#fff", textTransform: "uppercase", fontWeight: 'bold' }}>
+          Google Maps must be paid to work
+        </Text>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
